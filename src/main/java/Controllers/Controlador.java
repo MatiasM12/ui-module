81cc18e129f5cf3731f287b1f0d598fcc208ef89
Felipe Corso <pipecorso79@gmail.com>
2023-03-29 10:41:05 -0300

    nuevo Main + detalles controlador

--- src/main/java/Controllers/Controlador.java
+++ src/main/java/Controllers/Controlador.java
@@ -1,5 +1,13 @@
 package Controllers;
 
+import Views.Vista;
+import core.RepoTestFinder;
+
 public class Controlador {
+    Vista vista1;
+    RepoTestFinder core;
+    public void Controlador() {
+
+    }
 
 }
---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+
+import Views.Vista;
+
+public class Main {
+
+    public static void Main (String[] args){
+        RepoTestFinder coreX = new RepoTestFinder();
+        Vista testEnSwing = new Vista();
+
+    }
+}
