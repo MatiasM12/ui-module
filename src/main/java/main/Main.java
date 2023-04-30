1ac34b69d7ab83a6f90a9b140cf72b574b1549a4
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-04-30 20:34:45 -0300

    Agregado el main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,26 +1,36 @@
 package main;
 
-import controllers.ReportController;
-import core.PvCore;
-import core.ReportContainerAssembler;
-import core.ReportResultAssembler;
+import core.Core;
+import core.InitCore;
 import views.ReportView;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
+	static String trackerDirectoryPath = "C:\\Users\\Usuario\\IntelliJ-Workspace\\VALKIRIA\\core-module\\build\\classes\\java\\main\\InterfacesImpl";
+	static String trackerImpl = "InterfacesImpl.DefaultReportTracker";
+	static String reportDirectoryPath = "reportPath";
 
-	 public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+		InitCore initCore = new InitCore(trackerDirectoryPath);
+		Core core = initCore.init(trackerImpl,reportDirectoryPath);
 
-		ReportResultAssembler reportAssembler = new ReportResultAssembler();
-		ReportContainerAssembler containerAssembler = new ReportContainerAssembler();
-		PvCore pv = new PvCore();
-		pv.init(reportAssembler, containerAssembler);
-		
-		ReportView view = new ReportView();
-		
-		ReportController controller = new ReportController(containerAssembler.container,view);
-		
-		//le llegue el update
-		pv.update();
-	 }
+		ReportView view = new ReportView(core);
 
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					core.refresh();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}, new Date(), 5000);
+	}
 }
