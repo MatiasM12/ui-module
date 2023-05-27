7960b10256d730119e30b99ec87a58ca7fb8b58f
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-05-27 13:40:10 -0300

    commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,10 @@
 package main;
 
-import core.InitCore;
 import core.InitProviderTS;
-import core.Mediador;
 import core.Provider;
 import views.ReportView;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class Main {
@@ -19,13 +13,14 @@ public class Main {
 		//** Aca creo la vista
 		//devuelvo el string
 		//se lo paso por parametro al init core
-		String pluginsImplPath = "plugins";
+		String pluginsImplPath = "Factory";
 		String US = "US1";
-		String pluginImpl = "pluginImpl";
+		String pluginImpl = "PluginWithTimerFactory";
 
 		InitProviderTS initProviderTS = new InitProviderTS();
-		Provider provider = initProviderTS.init(new String[]{US, pluginsImplPath});
+		Provider provider = initProviderTS.init(new String[]{US,"",pluginsImplPath});
 
 		ReportView view = new ReportView(provider);
+	}
 		
 }
